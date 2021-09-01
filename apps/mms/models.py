705ed44ef21e9b501de93a4df82572449d3253e3
Anderson Bairo <anderson.bairo@yahoo.com.br>
2021-08-31 22:37:43 -0300

    cria e configura model MMS

--- apps/mms/models.py
+++ apps/mms/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class MMS(models.Model):
+    pair = models.CharField(max_length=6)
+    timestamp = models.DateTimeField()
+    mms_20 = models.DecimalField(max_digits=10, decimal_places=2)
+    mms_50 = models.DecimalField(max_digits=10, decimal_places=2)
+    mms_200 = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.pair} - {self.timestamp}'
+
+    class Meta:
+        verbose_name = 'MMS'
+        verbose_name_plural = 'MMS'
