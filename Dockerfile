f7de9ed7feaf924de5ce6bc45648440d4a7a4bdd
Anderson Bairo <anderson.bairo@yahoo.com.br>
2021-08-31 21:43:50 -0300

    configura docker, decouple e postgresql

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+FROM python:3.9.7-alpine3.14
+
+ARG DJANGO_ENV
+
+ENV DJANGO_ENV=${DJANGO_ENV} \
+  # python:
+  PYTHONFAULTHANDLER=1 \
+  PYTHONUNBUFFERED=1 \
+  PYTHONHASHSEED=random \
+  # pip:
+  PIP_NO_CACHE_DIR=off \
+  PIP_DISABLE_PIP_VERSION_CHECK=on \
+  PIP_DEFAULT_TIMEOUT=100 \
+  # poetry:
+  POETRY_VERSION=1.1.8 \
+  POETRY_VIRTUALENVS_CREATE=false
+
+# System deps:
+RUN apk --no-cache add \
+    rust \
+    cargo \
+    gcc \
+    libffi-dev \
+    openssl-dev \
+    postgresql-dev \
+    bash \
+  && pip install "poetry==$POETRY_VERSION" 
+
+
+# Create a group and user
+RUN addgroup -S appgroup && adduser -S docker -G appgroup
+
+# Tell docker that all future commands should run as the appuser user
+USER docker
+
+# Copy only requirements, to cache them in docker layer:
+COPY pyproject.toml poetry.lock /app/
+WORKDIR /app
+RUN poetry install $(test "$DJANGO_ENV" == production && echo "--no-dev") --no-interaction --no-ansi
+
+COPY . /app
--- mercadobitcoin/settings.py
+++ mercadobitcoin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1!#cle!&2p_d!y4riqb7(j=#4ai+p*5u07kw=gzg#doqen^715'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -75,11 +76,14 @@ WSGI_APPLICATION = 'mercadobitcoin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT', cast=int),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
