e6c944e3c3166d963eb9d887a66b0fdcfaa217d1
Anderson Bairo <anderson.bairo@yahoo.com.br>
2021-09-07 20:20:36 -0300

    closes #3: configura servidor asgi daphne

--- mercadobitcoin/settings.py
+++ mercadobitcoin/settings.py
@@ -73,6 +73,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mercadobitcoin.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
